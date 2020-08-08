9647c8da7b3bedec49d69248f32c37de9aab964e
brandonreid <brandonreidak@gmail.com>
2020-08-08 07:56:29 -0700

    test config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   url: 'https://brandonreid.me/',
-  pathPrefix: '/',
+  pathPrefix: '/portfolio-2020s',
   title: 'Portfolio for Brandon Reid',
   subtitle: 'Brandon Reid is a designer that can code.',
   copyright: '© All rights reserved.',
