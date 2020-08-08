ff7384a66c8fc15a32f6247507039fb8ead19d55
brandonreid <brandonreidak@gmail.com>
2020-08-08 10:02:05 -0700

    remove scrollintoview behavior

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styles from './Page.module.scss';
 
 type Props = {
@@ -6,15 +6,8 @@ type Props = {
   children: React.Node
 };
 
-const Page = ({ title, children }: Props) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    pageRef.current.scrollIntoView();
-  });
-
-  return (
-    <div ref={pageRef} className={styles['page']}>
+const Page = ({ title, children }: Props) => (
+    <div className={styles['page']}>
       <div className={styles['page__inner']}>
         { title && <h1 className={styles['page__title']}>{title}</h1>}
         <div className={styles['page__body']}>
@@ -22,7 +15,6 @@ const Page = ({ title, children }: Props) => {
         </div>
       </div>
     </div>
-  );
-};
+);
 
 export default Page;
